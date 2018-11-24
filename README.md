59cc72cea2d4bfdb94f8011e48fafaff54b53ee0
Andrew <arellok@gmail.com>
2018-11-24 00:14:13 -0800

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # eth-token-scraper
+Intended to concurrently grab all holders of an ERC20 token and output the balances
+they hold.
+
+Set env var `CONTRACT_ADDRESS=""` to the ERC20 contract you wish to scrap and
+`ETH_NETWORK=""` to specify the network.
+
+# Example
+`CONTRACT_ADDRESS="0xcba0b17f1afa724d2a19c040d7f90f0468b662ea"` \
+`ETH_NETWORK=rinkeby`
+
+```
+"0x26da0a36d60ef200"
+"0x26da0a36d60ef200"
+"0xae771444348cc00"
+"0x1043561a882930000"
+"0x298cf01478215600"
+```
---
+++ src/main.rs
@@ -0,0 +1,109 @@
+extern crate reqwest;
+extern crate env_logger;
+extern crate url;
+extern crate serde_json;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate base64;
+
+use std::thread;
+use std::time::Duration;
+use std::env;
+
+use url::Url;
+
+use serde_json::{Value};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct LogResponse {
+    id: i32,
+    jsonrpc: String,
+    result: Vec<Value>
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BalanceResponse {
+    id: i32,
+    jsonrpc: String,
+    result: String
+}
+
+
+fn get_balance(addr: String) -> Result<BalanceResponse, Box<std::error::Error>> {
+    let parsed_adder = format!(
+        "0x{}", &addr[27..67]
+    );
+    let params = format!(
+        "params=[\
+            \"{}\",\
+            \"{}\"\
+        ]",
+        parsed_adder,
+        "latest"
+    );
+    let url = format!("https://api.infura.io/v1/jsonrpc/{}/eth_getBalance?{}",
+                       env::var("ETH_NETWORK").unwrap(), params);
+    let url = Url::parse(&url)?;
+    let mut res = reqwest::get(url)?;
+    let text = res.text()?;
+    let v: BalanceResponse = match serde_json::from_str(&text){
+        Result::Ok(val) => {val},
+        Result::Err(err) => {panic!("Unable to parse json: {}",err)}
+    };
+
+    println!("{:?}", v.result);
+    Ok(v)
+}
+
+
+fn get_logs() -> Result<LogResponse, Box<std::error::Error>> {
+    let params = format!(
+        "params=[\
+            {{\
+                \"address\": \"{}\",\
+                \"fromBlock\": \"{}\",\
+                \"toBlock\": \"{}\",\
+                \"topics\": [\"{}\"]\
+            }}\
+        ]",
+        env::var("CONTRACT_ADDRESS").unwrap(),
+        "0x305FC6",
+        "latest",
+        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+    );
+    let url = format!("https://api.infura.io/v1/jsonrpc/{}/eth_getLogs?{}",
+                      env::var("ETH_NETWORK").unwrap(), params);
+    let url = Url::parse(&url)?;
+
+    let mut res = reqwest::get(url)?;
+    let text = res.text()?;
+    let v: LogResponse = match serde_json::from_str(&text){
+        Result::Ok(val) => {val},
+        Result::Err(err) => {panic!("Unable to parse json: {}",err)}
+    };
+    Ok(v)
+}
+
+
+fn main() {
+    let mut thread_vector = vec![];
+    let logs = get_logs().unwrap();
+    let result: Vec<Value> = logs.result;
+
+    for entry in result {
+        thread_vector.push(
+            thread::spawn(
+                move || { get_balance(entry["topics"][2].to_string()); thread::sleep(Duration::from_millis(1)); }
+            )
+        );
+    }
+    for child in thread_vector {
+       match child.join() {
+          Ok(_) => (),
+          Err(why) => println!("Join failure {:?}", why),
+       };
+   }
+}
