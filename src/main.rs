5da8efbc92608a3ee1883870cdfd8ac4ed77c858
Andrew <arellok@gmail.com>
2018-11-28 19:23:19 -0800

    Add mongo, overhaul process

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate reqwest;
+#[macro_use]
+extern crate log;
 extern crate env_logger;
 extern crate url;
 extern crate serde_json;
@@ -6,6 +8,12 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate base64;
+extern crate mongodb;
+
+use mongodb::{bson, doc};
+use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+use mongodb::coll::options::UpdateOptions;
 
 use std::thread;
 use std::time::Duration;
@@ -33,21 +41,31 @@ struct BalanceResponse {
     result: String
 }
 
+static BALANCE_SHA3: &str = "0x70a08231000000000000000000000000";
+const UPDATE_OPTIONS: Option<UpdateOptions> = Some(
+    UpdateOptions {upsert: Some(true), write_concern: None});
+
 
 fn get_balance(address: Vec<String>) -> Result<(), Box<std::error::Error>> {
     for addr in address {
         let parsed_adder = format!(
+            "{}{}", BALANCE_SHA3, &addr[27..67]
+        );
+        let stored_addr = format!(
             "0x{}", &addr[27..67]
         );
         let params = format!(
             "params=[\
-                \"{}\",\
-                \"{}\"\
+                {{\
+                    \"to\": \"{}\",\
+                    \"data\": \"{}\"\
+                }},\
+                \"latest\"\
             ]",
-            parsed_adder,
-            "latest"
+            env::var("CONTRACT_ADDRESS").unwrap(),
+            parsed_adder
         );
-        let url = format!("https://api.infura.io/v1/jsonrpc/{}/eth_getBalance?{}",
+        let url = format!("https://api.infura.io/v1/jsonrpc/{}/eth_call?{}",
                            env::var("ETH_NETWORK").unwrap(), params);
         let url = Url::parse(&url)?;
         let mut res = reqwest::get(url)?;
@@ -56,15 +74,31 @@ fn get_balance(address: Vec<String>) -> Result<(), Box<std::error::Error>> {
             Result::Ok(val) => {val},
             Result::Err(err) => {panic!("Unable to parse json: {}",err)}
         };
+        let result: String = v.result;
+        if result == "0x0" {
+            continue
+        };
+        let client = Client::connect("localhost", 27017)
+            .expect("Failed to initialize standalone client.");
 
-        println!("{:?}", v.result);
+        let coll = client.db(&env::var("MONGO_DB_NAME").unwrap()).collection("addresses");
+
+        let filter_doc = doc! {
+            "address": stored_addr,
+        };
+        let update_doc = doc! {
+            "$set" => {"balance": result}
+        };
+
+        coll.update_one(filter_doc, update_doc, UPDATE_OPTIONS)
+            .ok().expect("Failed to insert document.");
 
     }
     Ok(())
 }
 
 
-fn get_logs() -> Result<LogResponse, Box<std::error::Error>> {
+fn get_logs(from_block: String) -> Result<LogResponse, Box<std::error::Error>> {
     let params = format!(
         "params=[\
             {{\
@@ -75,7 +109,7 @@ fn get_logs() -> Result<LogResponse, Box<std::error::Error>> {
             }}\
         ]",
         env::var("CONTRACT_ADDRESS").unwrap(),
-        "0x60998B",
+        from_block,
         "latest",
         "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
     );
@@ -94,14 +128,53 @@ fn get_logs() -> Result<LogResponse, Box<std::error::Error>> {
 
 
 fn main() {
-    let mut thread_vector = vec![];
-    let logs = get_logs().unwrap();
+    /*
+        Connect to mongo and retrieve the last block used.
+    */
+    let client = Client::connect("localhost", 27017)
+        .expect("Failed to initialize standalone client.");
+    let coll = client.db(&env::var("MONGO_DB_NAME").unwrap()).collection("last_block");
+    let cursor = coll.find_one(None, None).unwrap();
+    let doc = cursor.expect("None found");
+    let from_block: String = doc.get("last_block")
+        .unwrap()
+        .to_string()
+        .replace("\"", "");
+    info!("Using block: {:?}", from_block);
+
+    /*
+        Process the events in the contract and retrieve the block of the last event.
+        Then update the new latest block in the db.
+    */
+    let logs = get_logs(from_block.clone()).unwrap();
     let result: Vec<Value> = logs.result;
     let mut addresses: HashSet<String> = HashSet::new();
+    let latest_block = result[result.len() - 1]["blockNumber"].to_string();
+    let filter_doc = doc! {
+        "last_block": from_block.clone(),
+    };
+    let update_doc = doc! {
+        "$set" => {"last_block": latest_block}
+    };
+    coll.update_one(filter_doc, update_doc, UPDATE_OPTIONS)
+        .ok().expect("Failed to insert document.");
+
+    /*
+        Convert the HashSet into a Vector of unique addresses (token holders).
+        At this point, addresses can contain a 0x0 balance so the length of
+        the vector will not be accurate.
+    */
     for entry in result {
         addresses.insert(entry["topics"][2].to_string());
     }
     let mut address_vec: Vec<_> = addresses.drain().collect();
+
+    /*
+        Create a vector to store the spawned threads. Threads are chunked into groups of 5
+        addresses by default. This value may need to change depending on amount of addresses
+        vector or operating system restrictions.
+    */
+    let mut thread_vector = vec![];
     while !address_vec.is_empty() {
         let end = match 3.cmp(&address_vec.len()) {
             Ordering::Less => 3,
@@ -111,7 +184,10 @@ fn main() {
         let u: Vec<_> = address_vec.drain(0..end).collect();
         thread_vector.push(
             thread::spawn(
-                move || { get_balance(u); thread::sleep(Duration::from_millis(1)); }
+                move || {
+                    get_balance(u).expect("Get balance failed");
+                    thread::sleep(Duration::from_millis(1));
+                }
             )
         );
     }
